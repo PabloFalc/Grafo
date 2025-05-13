b395a327d8836ebd2487f8a3dfa464a55957aaa3
PabloFalc <pablopatricio640@gmail.com>
2025-05-12 23:14:31 -0300

    fix: alguns conflitos resolvidos, agora irei mimir

--- src/main/java/simulador/Main.java
+++ src/main/java/simulador/Main.java
@@ -4,8 +4,6 @@ import estruturas.lista.Lista;
 import gerador.GeradorMapa;
 import grafo.Aresta;
 import grafo.Grafo;
-
-import grafo.Aresta;
 import grafo.Vertice;
 
 import javafx.application.Application;
@@ -16,9 +14,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import grafo.Vertice;
-import grafo.Aresta;
-import veiculo.veiculo;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,17 +28,7 @@ public class Main extends Application {
         Lista<Aresta> arestaLista = grafo.getArestas();
         System.out.println(arestaLista.tamanho);
 
-         Vertice v1 = new Vertice(100, 100, "A");
-          Vertice v2 = new Vertice(200, 150, "B");
-          Vertice v3 = new Vertice(350, 125, "C");
-          Vertice v4 = new Vertice(400, 50, "D");
 
-          Aresta a1 = new Aresta(v1, v2, 5, 0.1, false, 50);
-          Aresta a2 = new Aresta(v2, v3, 10, 0.2, true, 60);
-          Aresta a3 = new Aresta(v3, v4, 15, 0.3, false, 70);
-          Aresta a4 = new Aresta(v4, v1, 20, 0.4, true, 80);
-          Aresta a5 = new Aresta(v1, v3, 25, 0.5, false, 90);
-          Aresta a6 = new Aresta(v2, v4, 30, 0.6, true, 100);
 
 
 
@@ -51,6 +37,21 @@ public class Main extends Application {
   
     @Override
     public void start(Stage stage) {
+
+        Vertice v1 = new Vertice(100, 100, "A");
+        Vertice v2 = new Vertice(200, 150, "B");
+        Vertice v3 = new Vertice(350, 125, "C");
+        Vertice v4 = new Vertice(400, 50, "D");
+
+        Aresta a1 = new Aresta("a", 50, false, 0.1, 5, v1, v2);
+        Aresta a2 = new Aresta("b", 60, true, 0.2, 10, v2, v3);
+        Aresta a3 = new Aresta("c", 70, false, 0.3, 15, v3, v4);
+        Aresta a4 = new Aresta("d", 80, true, 0.4, 20, v4, v1);
+        Aresta a5 = new Aresta("e", 90, false, 0.5, 25, v1, v3);
+        Aresta a6 = new Aresta("f", 2, true, 2, 30, v2, v4);
+
+
+
         Pane pane = new Pane();
 
         Vertice[] vertices = {v1, v2, v3, v4};
@@ -71,7 +72,8 @@ public class Main extends Application {
 
         // Adicionar arestas ao Pane
         for (Aresta a : arestas) {
-            Line line = new Line(a.getSource().getLongitude(), a.getSource().getLatitude(), a.getTarget().getLongitude(), a.getTarget().getLatitude());
+            Line line = new Line(a.getDestino().getLongitude(), a.getOrigem().getLatitude(), a.getDestino().getLongitude(),
+                    a.getDestino().getLatitude());
             line.setStroke(Color.GRAY);
             line.setStrokeWidth(2);
             pane.getChildren().add(0, line); // Adiciona atrás dos vértices
